3d00c111495aed439142dc86d5da99f31f6c8b8f
Mathias Preiner <mathias.preiner@gmail.com>
2026-05-04 20:13:13 +0000

    type: Fix FP type cardinality computation.

--- src/type/card.cpp
+++ src/type/card.cpp
@@ -38,8 +38,8 @@ compute_cardinality(const Type& type)
     // +zero -zero: 2
     // +inf  -inf : 2
     // nan        : 1
-    // subnormals : 2 * (2^{sig_size-1)-1) = 2^{sig_size} - 2
-    // normalss   : 2 * (2^{exp_size} - 2) * 2^{sig_size-1} =
+    // subnormals : 2 * (2^{sig_size-1}-1) = 2^{sig_size} - 2
+    // normals    : 2 * (2^{exp_size} - 2) * 2^{sig_size-1} =
     //              (2^{exp_size} - 2) * 2^{sig_size}
     //
     // card = 5 + 2^{sig_size} - 2 + (2^{exp_size} - 2) * 2^{sig_size}
--- src/type/type.cpp
+++ src/type/type.cpp
@@ -11,6 +11,7 @@
 #include "type/type.h"
 
 #include <cassert>
+#include <sstream>
 
 #include "printer/smt2_printer.h"
 #include "type/type_data.h"
