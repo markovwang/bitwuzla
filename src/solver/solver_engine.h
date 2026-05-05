58430253d7b02bd564946782a242ccc4bd5ac876
Mathias Preiner <mathias.preiner@gmail.com>
2026-05-05 16:24:27 -0700

    solver: engine: Add distinct_n lemma statistics.

--- src/solver/solver_engine.h
+++ src/solver/solver_engine.h
@@ -203,6 +203,7 @@ class SolverEngine
     uint64_t& num_lemmas_fun;
     uint64_t& num_lemmas_quant;
     uint64_t& num_lemmas_abstr;
+    uint64_t& num_lemmas_distinctn;
     util::TimerStatistic& time_register_term;
     util::TimerStatistic& time_solve;
   } d_stats;
